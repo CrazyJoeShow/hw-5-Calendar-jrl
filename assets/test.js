7aac86c1f523e1a36b5f6c636982b261b73247a7
Joe Lowery <joseph.loweryit@gmail.com>
2020-09-12 23:08:49 -0400

    removed some psuedo and added a little to ReadMe

--- assets/test.js
+++ assets/test.js
@@ -1,13 +1,1 @@
 console.log('henlo Frins');
-
-var hourArray = [
-	moment('9:00am', 'h A'),
-	moment('10:00am', 'h A'),
-	moment('11:00am', 'h A'),
-	moment('12:00pm', 'h A'),
-	moment('1:00pm', 'h A'),
-	moment('2:00pm', 'h A'),
-	moment('3:00pm', 'h A'),
-	moment('4:00pm', 'h A'),
-	moment('5:00pm', 'h A'),
-];
