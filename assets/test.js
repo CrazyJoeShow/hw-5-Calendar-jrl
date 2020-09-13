dc6a0957b2586863239c9270d9511f3f08244066
Joe Lowery <joseph.loweryit@gmail.com>
2020-09-12 20:52:50 -0400

    assets folder for calls to script

---
+++ assets/test.js
@@ -0,0 +1,14 @@
+console.log("hello world")
+
+var hourArray = [
+    moment("9:00am", "h A"),
+    moment("10:00am", "h A"),
+    moment("11:00am", "h A"),
+    moment("12:00pm", "h A"),
+    moment("1:00pm", "h A"),
+    moment("2:00pm", "h A"),
+    moment("3:00pm", "h A"),
+    moment("4:00pm", "h A"),
+    moment("5:00pm", "h A"),
+]
+
