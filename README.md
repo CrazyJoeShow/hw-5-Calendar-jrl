9cb96dd196a7475529e3da546be51bbe04fe5619
Joe Lowery <joseph.loweryit@gmail.com>
2020-09-12 22:59:16 -0400

    trying to fix clock showing hours red for 10PM and 11PM

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # hw-5-Calendar-jrl
+
+### This is a day planner for the hours of 9am-5pm
+
+## ==============================================================
+
+The application is rather simple in a sense of the user is just adding to-do's to a pre-set hour block. On the flip-side I spent countless hours in the sandbox trying to figure out how to get syntax for moment.js correct in how I was trying to use it.
+
+I used the array pulled from storage to dynamically generate the users content inthe corresponding block, because it's already there in the storage i just need to essentially save it, call it, and append to the page.
+
+Lots of dissecting other's code with different attack approaches with the time and saving the users input and displaying.
+The Office hours prior to and after class were quite helpful this week as there were a couple questions pertaining to this project.
+
+### =============================================================
--- assets/real.js
+++ assets/real.js
@@ -1,23 +1,27 @@
-console.log('henlo frins!');
+console.log('henlo frinss');
 $(document).ready(function () {
-	//Golbals
+	// GLOBAL VARIABLES
 	var currentDateText = $('#current-day');
-	console.log(currentDateText);
+	// console.log(currentDateText);
 
 	var mainContainer = $('.main');
-	console.log(mainContainer);
+	// console.log(mainContainer);
+	// MOMENTS
 
 	let m = moment();
+
 	console.log(m.hour());
+
 	let currentHour = moment().format('h');
 	console.log(currentHour);
-	//24 hour clock
-	if (currentHour < 10) {
-		console.log(currentHour + 'is less than');
-	}
-	currentDateText.text(moment().format('MMM ddd YYYY'));
 
-	// Object for divs
+	//looping m24 hour clock
+	if (currentHour < 10) {
+		console.log(currentHour + ' is less than!');
+	}
+
+	currentDateText.text(moment().format('MMM ddd do YYYY'));
+
 	var initialToDoStorage = {
 		'9am': '',
 		'10am': '',
@@ -29,7 +33,7 @@ $(document).ready(function () {
 		'4pm': '',
 		'5pm': '',
 	};
-	// hour list and keys
+
 	var hourList = [
 		'9am',
 		'10am',
@@ -41,14 +45,15 @@ $(document).ready(function () {
 		'4pm',
 		'5pm',
 	];
+
 	var momentHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
-	// Parse of the local storage.
+	// local storage .parse
 	var initialStorage =
 		JSON.parse(localStorage.getItem('agendaList')) || initialToDoStorage;
 	console.log(initialStorage);
 
-	// saving the current events
+	//  saving the events
 	mainContainer.on('click', '.saveBtn', function () {
 		console.log($(this).siblings('div').text()); //this works because we need to reference a SIBLING in the same container.
 		console.log($(this).siblings('textarea').val());
@@ -64,21 +69,21 @@ $(document).ready(function () {
 	});
 
 	for (var i = 0; i < momentHours.length; i++) {
-		var timeRow = '<row>' + '</row>';
+		var timeRow = $('<row>' + '</row>');
 		timeRow.addClass('row time-block');
 		mainContainer.append(timeRow);
-		//that should add rows for time blocks
 
 		var hourContainer = $('<div>');
 		hourContainer.addClass('col-2 hour');
 		hourContainer.text(hourList[i]);
 		timeRow.append(hourContainer);
 
-		// forms for user input
-		var formContainer = $('<div>');
-		hourContainer.addClass('text-area col-8');
-		formContainer.attr('placeholder', 'This hour is open' + hourList[i]);
+		// form container for the input
+		var formContainer = $('<textarea>');
+		formContainer.addClass('text-area col-8');
+		formContainer.attr('placeholder', 'Open' + hourList[i]);
 
+		//class added to the Container
 		if (currentHour < momentHours[i]) {
 			formContainer.addClass('future');
 		} else if (currentHour > momentHours[i]) {
@@ -88,5 +93,10 @@ $(document).ready(function () {
 		formContainer.attr('id', [i]);
 		formContainer.text(initialStorage[hourList[i]]);
 		timeRow.append(formContainer);
+
+		var formSave = $('<div>' + ' SAVE' + '</div>');
+		formSave.addClass('saveBtn col-2 fas fa-save');
+		formSave.attr('dataType', hourList[i]);
+		timeRow.append(formSave);
 	}
 });
